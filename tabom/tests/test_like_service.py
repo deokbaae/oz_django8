99ec2528a74e90e0c119b251a957848e012128fa
SW <aliwo@naver.com>
2025-02-27 15:10:31 +0900

    prevent duplicate likes

--- tabom/tests/test_like_service.py
+++ tabom/tests/test_like_service.py
@@ -1,5 +1,6 @@
-from dataclasses import asdict
+from http.client import HTTPException
 
+from django.db import IntegrityError
 from django.test import TestCase
 
 from tabom.models import Article, User
@@ -19,3 +20,13 @@ class TestLikeService(TestCase):
         self.assertIsNotNone(like.id)
         self.assertEqual(user.id, like.user_id)
         self.assertEqual(article.id, like.article_id)
+
+    def test_a_user_can_like_an_article_only_once(self) -> None:
+        # Given
+        user = User.objects.create(name="test")
+        article = Article.objects.create(title="test_title")
+
+        # Expect
+        like1 = do_like(user.id, article.id)
+        with self.assertRaises(IntegrityError):
+            like2 = do_like(user.id, article.id)
